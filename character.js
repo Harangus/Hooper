2c30d0fc124fbf495ac795a40ca83aa81e1a6f3f
Harangus <vencacvrcov@gmail.com>
2024-05-31 12:38:03 +0200

    skakani

--- character.js
+++ character.js
@@ -1,16 +1,24 @@
-class Character
-{
-    constructor(width, height, characterPosition, color)
-    {
-        this.width = width;
-        this.height = height;
-        this.characterPosition = characterPosition;
-        this.color = color;
-    }
+class Character {
+  constructor(width, height, characterPosition, color) {
+    this.width = width;
+    this.height = height;
+    this.characterPosition = characterPosition;
+    this.color = color;
+    this.velocity = 0;
+    this.weight = 0.1;
+    this.gravityEnabled = true;
+    this.jumpPower = 90;
+    this.jumpEnabled = false;
+    this.jumpedDistance = 0;
+  }
 
-    drawCharacter()
-    {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.characterPosition.x, this.characterPosition.y, this.width, this.height);
-    }
+  drawCharacter() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(
+      this.characterPosition.x,
+      this.characterPosition.y,
+      this.width,
+      this.height
+    );
+  }
 }
--- main.js
+++ main.js
@@ -1,13 +1,50 @@
 let c = document.getElementById("gameCanvas");
 let ctx = c.getContext("2d");
-//let gameSpeed = setInterval(gameLoop, 1000/60);
+let gameSpeed = setInterval(gameLoop, 1000 / 90);
 
-let player = new Character(20, 40, {x: 200, y: 100}, "blue");
+let player = new Character(20, 40, { x: 200, y: 100 }, "blue");
 player.drawCharacter();
 
-function gameLoop()
-{
-    ctx.clearRect(0, 0, c.width, c.height);
-    player.characterPosition.x += 1;
-    player.drawCharacter();
+function gameLoop() {
+  ctx.clearRect(0, 0, c.width, c.height); //Vyčištění obrazu
+  if (player.gravityEnabled) {
+    gravity();
+  }
+  if (player.jumpEnabled) {
+    jump();
+  }
+  player.drawCharacter();
 }
+
+function gravity() {
+  if (player.characterPosition.y < c.height - player.height) {
+    player.characterPosition.y += player.velocity + player.weight;
+    player.velocity += 0.1;
+  }
+
+  if (player.characterPosition.y == c.height - player.height) {
+    player.velocity = 0;
+  }
+}
+
+function jump() {
+    if (player.jumpedDistance < player.jumpPower) {
+        player.characterPosition.y -= player.velocity + player.jumpPower / 30
+        player.jumpedDistance += player.velocity + player.jumpPower / 30;
+        player.velocity -= 1;
+    }else
+    {
+        player.jumpEnabled = false;
+        player.velocity = 0;
+        player.gravityEnabled = true;
+        player.jumpedDistance = 0;
+    }
+}
+
+document.addEventListener("keydown", function (event) {
+  if (event.keyCode == 32) {
+    player.velocity = 10;
+    player.gravityEnabled = false;
+    player.jumpEnabled = true;
+  }
+});
