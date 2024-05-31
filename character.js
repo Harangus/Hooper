4afeb357f1f24cd089c09cb65c05690d68896220
Harangus <vencacvrcov@gmail.com>
2024-05-31 18:04:08 +0200

    sprite, pohybujici se pozadi

--- character.js
+++ character.js
@@ -1,24 +1,26 @@
 class Character {
-  constructor(width, height, characterPosition, color) {
+  constructor(width, height, characterPosition) {
     this.width = width;
     this.height = height;
     this.characterPosition = characterPosition;
-    this.color = color;
     this.velocity = 0;
     this.weight = 0.1;
     this.gravityEnabled = true;
     this.jumpPower = 90;
     this.jumpEnabled = false;
     this.jumpedDistance = 0;
+    this.jumpCompleted = false;
+    this.sprite = new Image();
+    this.sprite.src = 'https://seeklogo.com/images/D/dinosaur-game-logo-2723F385F0-seeklogo.com.png';
   }
 
   drawCharacter() {
-    ctx.fillStyle = this.color;
-    ctx.fillRect(
+    /*ctx.strokeRect(
       this.characterPosition.x,
       this.characterPosition.y,
       this.width,
       this.height
-    );
+    );*/
+    ctx.drawImage(this.sprite, this.characterPosition.x, this.characterPosition.y, this.width, this.height);
   }
 }
--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 let c = document.getElementById("gameCanvas");
 let ctx = c.getContext("2d");
 let gameSpeed = setInterval(gameLoop, 1000 / 90);
+let background = new Image();
+background.src = 'https://media.istockphoto.com/id/1333010525/vector/simple-flat-pixel-art-illustration-of-cartoon-outdoor-landscape-background-pixel-arcade.jpg?s=612x612&w=0&k=20&c=uTGqB9fhmjzaNd17EGRHYU04_70K7a3M8ilRoJjDwtY=';
+let backgroundPosition = {x: 0, y: 0};
+let background2 = new Image();
+background2.src = 'https://media.istockphoto.com/id/1333010525/vector/simple-flat-pixel-art-illustration-of-cartoon-outdoor-landscape-background-pixel-arcade.jpg?s=612x612&w=0&k=20&c=uTGqB9fhmjzaNd17EGRHYU04_70K7a3M8ilRoJjDwtY=';
 
-let player = new Character(20, 40, { x: 200, y: 100 }, "blue");
+let background2Position = {x: 1000, y: 0};
+
+let player = new Character(80, 80, { x: 200, y: 100 }); 
 player.drawCharacter();
 
 function gameLoop() {
-  ctx.clearRect(0, 0, c.width, c.height); //Vyčištění obrazu
+  //ctx.clearRect(0, 0, c.width, c.height); //Vyčištění obrazu
+  ctx.drawImage(background, backgroundPosition.x, backgroundPosition.y, c.width, c.height);
+  ctx.drawImage(background2, background2Position.x, background2Position.y, c.width, c.height);
   if (player.gravityEnabled) {
     gravity();
   }
@@ -14,16 +23,28 @@ function gameLoop() {
     jump();
   }
   player.drawCharacter();
+
+  backgroundPosition.x -= 1;
+  background2Position.x -= 1;
+  if (backgroundPosition.x < -999)
+    {
+      backgroundPosition.x = 1000;
+    }
+  if (background2Position.x < -999)
+    {
+      background2Position.x = 1000;
+    }
 }
 
 function gravity() {
-  if (player.characterPosition.y < c.height - player.height) {
+  if (player.characterPosition.y < c.height - 180) {
     player.characterPosition.y += player.velocity + player.weight;
     player.velocity += 0.1;
   }
 
-  if (player.characterPosition.y == c.height - player.height) {
+  if (player.characterPosition.y >= c.height - 180) {
     player.velocity = 0;
+    player.jumpCompleted = true;
   }
 }
 
@@ -43,8 +64,11 @@ function jump() {
 
 document.addEventListener("keydown", function (event) {
   if (event.keyCode == 32) {
-    player.velocity = 10;
-    player.gravityEnabled = false;
-    player.jumpEnabled = true;
+    if (player.jumpCompleted) {
+      player.jumpCompleted = false;
+      player.velocity = 10;
+      player.gravityEnabled = false;
+      player.jumpEnabled = true;
+    }
   }
 });
